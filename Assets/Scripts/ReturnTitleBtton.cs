760edec8a7f9cf2928963005080dbd321ba01402
Asou Gima <e235708@ie.u-ryukyu.ac.jp>
2025-06-12 15:48:14 +0900

    6/12

---
+++ Assets/Scripts/ReturnTitleBtton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturntitleButton : MonoBehaviour
+{
+    void Start()
+    {
+        // ボタンが押されたときに StartGame を呼び出す
+        GetComponent<Button>().onClick.AddListener(StartGame);
+    }
+
+    void StartGame()
+    {
+        // GameStartSceanに遷移
+        SceneManager.LoadScene("GameStartScean");
+    }
+}
