97a10948d8d5abbc7e466d406bd5bc02f41d7b16
Yunosuke Komi <e235736@ie.u-ryukyu.ac.jp>
2025-06-19 14:12:02 +0900

    fix:after-merge-from-test-test-branch
    test-test-branchとマージした後のエラー修正

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -237,7 +237,7 @@ public class UIManager : MonoBehaviour
             TowerPlacement.PlaceTowerAtGrid(selectedGridPosition);
             
             // 配置したグリッドセルを占有状態にする
-            GridCell[] gridCells = FindObjectsOfType<GridCell>();
+            GridCell[] gridCells = Object.FindObjectsByType<GridCell>(FindObjectsSortMode.None);
             foreach (GridCell cell in gridCells)
             {
                 if (cell.gridPosition == selectedGridPosition)
